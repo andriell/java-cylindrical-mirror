829fbd3c1d5f3e5fbbbaa1a926490fdfd873809b
andriell <root.andriell@gmail.com>
2018-07-20 16:24:29 +0300

    Start

---
+++ src/main/java/com/andriell/cm/Triangle.java
@@ -0,0 +1,12 @@
+package com.andriell.cm;
+
+public class Triangle {
+    double a;
+    double b;
+    double c;
+    double ab;
+    double bc;
+    double ac;
+
+
+}
--- src/main/java/com/andriell/cm/gui/MainFrame.java
+++ src/main/java/com/andriell/cm/gui/MainFrame.java
@@ -1,10 +1,20 @@
 package com.andriell.cm.gui;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
 
 public class MainFrame {
-    JFrame frame;
+    private JFrame frame;
+    private JFileChooser fileChooser;
+    private File dataFile;
+
+
     private JPanel rootPanel;
+    private JPanel topPanel;
+    private JPanel centerPanel;
+    private JButton buttonFile;
 
     public void show() {
         frame = new JFrame("Cylindrical mirror");
@@ -13,6 +23,17 @@ public class MainFrame {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(1252, 875);
         frame.setContentPane(rootPanel);
+
+        fileChooser = new JFileChooser();
+
+        buttonFile.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                int ret = fileChooser.showOpenDialog(rootPanel);
+                if (ret == JFileChooser.APPROVE_OPTION) {
+                    dataFile = fileChooser.getSelectedFile();
+                }
+            }
+        });
 
         frame.setVisible(true);
     }
