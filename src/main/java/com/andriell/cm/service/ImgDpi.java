e2c925672963561b737d854348826cc9c90bf025
andriell <root.andriell@gmail.com>
2018-10-19 17:34:01 +0300

    3d math

---
+++ src/main/java/com/andriell/cm/service/ImgDpi.java
@@ -0,0 +1,53 @@
+package com.andriell.cm.service;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+public class ImgDpi {
+    // Размеры листа в миллиметрах
+    private double width = 297;
+    private double height = 210;
+    private double dpi = 300;
+    private BufferedImage image;
+
+    public ImgDpi() {
+        image = new BufferedImage(mm2px(width, dpi), mm2px(height, dpi), BufferedImage.TYPE_INT_RGB);
+    }
+
+    public static int mm2px(double mm, double dpi) {
+        return (int) Math.round(mm /  25.4 * dpi);
+    }
+
+    public static double px2mm(int px, double dpi) {
+        return ((double) px) / dpi * 25.4;
+    }
+
+    public static double dpi(double mm, int px) {
+        return ((double) px) / mm / 25.4;
+    }
+
+    public static BufferedImage resize(BufferedImage img, int newW, int newH) {
+        Image tmp = img.getScaledInstance(newW, newH, Image.SCALE_SMOOTH);
+        BufferedImage dimg = new BufferedImage(newW, newH, BufferedImage.TYPE_INT_ARGB);
+        Graphics2D g2d = dimg.createGraphics();
+        g2d.drawImage(tmp, 0, 0, null);
+        g2d.dispose();
+        return dimg;
+    }
+
+    public void addImage(BufferedImage img, double dpi) {
+
+
+        Graphics2D d = (Graphics2D) image.getGraphics();
+        d.setColor(Color.WHITE);
+        d.fillRect(0, 0, image.getWidth(), image.getHeight());
+        d.drawImage(img, (image.getWidth() - img.getWidth()) / 2, (image.getHeight() - img.getHeight()) / 2,  null);
+    }
+
+    public void save(File file) throws IOException {
+        ImageIO.write(image, "png", file);
+    }
+}
