4875959f4d682d45a62f4c849128140920c042ca
andriell <root.andriell@gmail.com>
2018-10-19 16:32:47 +0300

    3d math

--- src/test/java/com/andriell/cm/math/CylindricalMirrorTest.java
+++ src/test/java/com/andriell/cm/math/CylindricalMirrorTest.java
@@ -14,7 +14,7 @@ public class CylindricalMirrorTest {
     @Test
     public void matrix() {
         CylindricalMirror mirror = new CylindricalMirror(new PlaneImageCell(10, 10, 2));
-        mirror.setPointEay(new Point(10000, 0, 10000));
+        mirror.setPointEay(new Point(1E7, 0, 1E7));
         Point2d[][] matrix = mirror.matrix();
         for (int x = 0; x < matrix.length; x++) {
             for (int y = 0; y < matrix[x].length; y++) {
@@ -27,7 +27,7 @@ public class CylindricalMirrorTest {
     @Test
     public void writeImage() throws Exception {
         CylindricalMirror mirror = new CylindricalMirror(new PlaneImageCell(1001, 1001, 100));
-        mirror.setPointEay(new Point(100000, 0, 100000));
+        mirror.setPointEay(new Point(1E7, 0, 1E7));
         mirror.writeImage(new File("CylindricalMirror.png"));
     }
 }
